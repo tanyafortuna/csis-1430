820ddcd56d516537f8a6337278e076d3fc385650
Tanya Fortuna <tanyafortuna24@gmail.com>
2024-10-30 15:41:49 -0600

    Add 5-minute website from class 10-29

---
+++ 5minwebsite/js/script10-29.js
@@ -0,0 +1,11 @@
+alert("Hello");
+
+var food = "pizza";
+var guess = prompt("Guess my favorite food!");
+
+// if (food == guess) alert("You win!");
+// else alert("You lose!");
+
+while (food != guess) guess = prompt("Nope, guess again!");
+
+alert("You win!");
