63d95513f8112e9ad9666f8649c95a91af6ff206
Tanya Fortuna <tanyafortuna24@gmail.com>
2024-11-16 22:55:16 -0700

    Add a transition to show the theme

--- madlibs/js/script.js
+++ madlibs/js/script.js
@@ -1,11 +1,11 @@
 var labelsCamping = [
   "Person's name",
   'Noun',
+  'Verb',
   'Emotion',
   'Verb',
   'Emotion',
   'Animal',
-  'Verb',
   'Adjective',
   'Verb (-ing)',
   'Adverb',
@@ -17,8 +17,10 @@ var labelsCamping = [
   'Language',
   'Noun (plural)',
 ];
+var labelsHospital = [];
+var labelsForest = [];
 
-var numbers = ['2', '3', '14', '9', '5', '8', '42'];
+var numbers = ['18', '3', '14', '7', '5', '8', '40'];
 var nouns = [
   'piano',
   'money',
@@ -101,7 +103,7 @@ var measuresOfTime = [
   'years',
   'decades',
 ];
-var colors = ['crimson', 'teal', 'magenta', 'gold', 'indigo', 'lime', 'tan'];
+var colors = ['crimson', 'teal', 'magenta', 'gold', 'violet', 'lime', 'tan'];
 var monsters = [
   'vampire',
   'zombie',
@@ -121,7 +123,12 @@ var languages = [
   'C++',
 ];
 
+function initializePage() {
+  addClass('background', 'campfire'); //TODO
+}
+
 function createForm() {
+  // TODO
   var formHTML = '<form action="javascript:void(0);">';
   formHTML +=
     '<button id="random" type="button" onclick="provideRandomWords();">Random words</button>';
@@ -141,81 +148,94 @@ function createForm() {
 }
 
 function updatePage() {
-  document.getElementById('random').removeAttribute('onclick');
-  document.getElementById('random').classList.add('unclickable');
-  document.getElementById('submit').removeAttribute('onclick');
-  document.getElementById('submit').classList.add('unclickable');
+  addClass('random', 'unclickable');
+  addClass('submit', 'unclickable');
+  removeAttribute('random', 'onclick');
+  removeAttribute('submit', 'onclick');
 
   for (var i = 0; i < 17; i++) {
-    document.getElementById('input-' + i).removeAttribute('onkeyup');
+    removeAttribute('input-' + i, 'onkeyup');
     document.getElementById('input-' + i).setAttribute('readonly', '');
   }
 
-  document.getElementById('secret').classList.add('shown');
-  document.getElementById('story').classList.add('campfire');
-  updateStory();
+  replaceTheme('The Camping Trip'); //TODO
+  addClass('background', 'campfire'); //TODO
+  addClass('output', 'transparent');
+  createStoryHTML();
+  typeStory(createStoryText());
 }
 
-function updateStory() {
+function createStoryHTML() {
+  var storyHTML = '';
+
+  for (var i = 0; i < 34; i = i + 2) {
+    storyHTML += '<span class="text" id="story-' + i + '"></span>';
+    storyHTML += '<span class="entry" id="story-' + (i + 1) + '"></span>';
+  }
+  storyHTML += '<span class="text" id="story-34"></span>';
+
+  document.getElementById('story-text').innerHTML = storyHTML;
+}
+
+function createStoryText() {
+  // TODO
+  var storyText;
   var entries = [];
   for (var i = 0; i < 17; i++) {
     entries[i] = document.getElementById('input-' + i).value;
   }
 
-  var storyHTML = '<div>';
+  storyText = 'This weekend I am going camping with ';
+  storyText += '<' + entries[0] + '>.';
+  storyText += ' I packed my lantern, sleeping bag, and ';
+  storyText += '<' + entries[1] + '>.';
+  storyText += ' If I ';
+  storyText += '<' + entries[2] + '>';
+  storyText += ' in my tent I will be so ';
+  storyText += '<' + entries[3] + '>.';
+  storyText += ' We are going to hike, fish, and ';
+  storyText += '<' + entries[4] + '>.';
+  storyText += ' I am ';
+  storyText += '<' + entries[5] + '>';
+  storyText += ' we might see a ';
+  storyText += '<' + entries[6] + '>,';
+  storyText += ' they are kind of dangerous. I have heard that the ';
+  storyText += '<' + entries[7] + '>';
+  storyText += ' lake is great for ';
+  storyText += '<' + entries[8] + '>.';
+  storyText += ' After that we will ';
+  storyText += '<' + entries[9] + '>';
+  storyText += ' hike through the forest for ';
+  storyText += '<' + entries[10] + '>';
+  storyText += ' ';
+  storyText += '<' + entries[11] + '>.';
+  storyText += ' If I see a ';
+  storyText += '<' + entries[12] + '>';
+  storyText += ' ';
+  storyText += '<' + entries[13] + '>';
+  storyText += ' while hiking, I am going to bring it home as a pet!';
+  storyText += ' At night we will tell ';
+  storyText += '<' + entries[14] + '>';
+  storyText += ' stories in ';
+  storyText += '<' + entries[15] + '>';
+  storyText += ' and roast ';
+  storyText += '<' + entries[16] + '>';
+  storyText += ' around the campfire!';
 
-  storyHTML += 'This weekend I am going camping with ';
-  storyHTML += '<span class="entry">' + entries[0] + '</span>';
-  storyHTML += '. I packed my lantern, sleeping bag, and ';
-  storyHTML += '<span class="entry">' + entries[1] + '</span>';
-  storyHTML += '. I am so ';
-  storyHTML += '<span class="entry">' + entries[2] + '</span>';
-  storyHTML += ' to ';
-  storyHTML += '<span class="entry">' + entries[3] + '</span>';
-  storyHTML += ' in a tent. I am ';
-  storyHTML += '<span class="entry">' + entries[4] + '</span>';
-  storyHTML += ' we might see a ';
-  storyHTML += '<span class="entry">' + entries[5] + '</span>';
-  storyHTML += ', they are kind of dangerous.';
-  storyHTML += ' We are going to hike, fish, and ';
-  storyHTML += '<span class="entry">' + entries[6] + '</span>';
-  storyHTML += '. I have heard that the ';
-  storyHTML += '<span class="entry">' + entries[7] + '</span>';
-  storyHTML += ' lake is great for ';
-  storyHTML += '<span class="entry">' + entries[8] + '</span>';
-  storyHTML += '. Then we will ';
-  storyHTML += '<span class="entry">' + entries[9] + '</span>';
-  storyHTML += ' hike through the forest for ';
-  storyHTML += '<span class="entry">' + entries[10] + '</span>';
-  storyHTML += ' ';
-  storyHTML += '<span class="entry">' + entries[11] + '</span>';
-  storyHTML += '. If I see a ';
-  storyHTML += '<span class="entry">' + entries[12] + '</span>';
-  storyHTML += ' ';
-  storyHTML += '<span class="entry">' + entries[13] + '</span>';
-  storyHTML += ' while hiking, I am going to bring it home as a pet!';
-  storyHTML += ' At night we will tell ';
-  storyHTML += '<span class="entry">' + entries[14] + '</span>';
-  storyHTML += ' ';
-  storyHTML += '<span class="entry">' + entries[15] + '</span>';
-  storyHTML += ' stories and roast ';
-  storyHTML += '<span class="entry">' + entries[16] + '</span>';
-  storyHTML += ' around the campfire!';
-  storyHTML += '</div>';
-
-  document.getElementById('story').innerHTML += storyHTML;
+  return storyText;
 }
 
 function provideRandomWords() {
+  // TODO
   var rand = Math.floor(Math.random() * 7);
 
   document.getElementById('input-0').value = names[rand];
   document.getElementById('input-1').value = nouns[rand];
-  document.getElementById('input-2').value = emotions[rand];
-  document.getElementById('input-3').value = verbs[rand];
-  document.getElementById('input-4').value = emotions[(rand + 1) % 7];
-  document.getElementById('input-5').value = animals[rand];
-  document.getElementById('input-6').value = verbs[(rand + 1) % 7];
+  document.getElementById('input-2').value = verbs[rand];
+  document.getElementById('input-3').value = emotions[rand];
+  document.getElementById('input-4').value = verbs[(rand + 1) % 7];
+  document.getElementById('input-5').value = emotions[(rand + 1) % 7];
+  document.getElementById('input-6').value = animals[rand];
   document.getElementById('input-7').value = adjectives[rand];
   document.getElementById('input-8').value = verbIngs[rand];
   document.getElementById('input-9').value = adverbs[rand];
@@ -252,4 +272,69 @@ function isFilledOut() {
   )
     document.getElementById('submit').classList.remove('unclickable');
   else document.getElementById('submit').classList.add('unclickable');
+}
+
+function typeStory(story, start = 2500, delay = 50) {
+  var letters = story.split('');
+  var num = 0;
+  var skips = 0;
+
+  for (var i = 0; i < letters.length; i++) {
+    if (letters[i] == '<') {
+      skips++;
+      num++;
+      setTimeout(
+        addClass,
+        start + delay * (i + 1 - skips),
+        'story-' + num,
+        'revealed'
+      );
+    } else if (letters[i] == '>') {
+      skips++;
+      num++;
+    } else {
+      setTimeout(
+        appendLetter,
+        start + delay * (i + 1 - skips),
+        letters[i],
+        'story-' + num
+      );
+    }
+  }
+}
+
+function replaceTheme(theme, delay = 50) {
+  var letters = theme.split('');
+  var textLength = letters.length;
+
+  for (var i = 0; i < textLength; i++) {
+    letters.pop();
+    setTimeout(updateInnerHTML, delay * (i + 1), letters.join(''), 'secret');
+  }
+  setTimeout(addClass, delay * textLength, 'secret', 'shown');
+  letters = theme.split('');
+  for (var i = 0; i < textLength; i++) {
+    setTimeout(
+      appendLetter,
+      delay * (textLength + i + 1),
+      letters[i],
+      'secret'
+    );
+  }
+}
+
+function appendLetter(letter, elementID) {
+  document.getElementById(elementID).innerHTML += letter;
+}
+
+function updateInnerHTML(letters, elementID) {
+  document.getElementById(elementID).innerHTML = letters;
+}
+
+function addClass(elementID, c) {
+  document.getElementById(elementID).classList.add(c);
+}
+
+function removeAttribute(elementID, a) {
+  document.getElementById(elementID).removeAttribute(a);
 }
